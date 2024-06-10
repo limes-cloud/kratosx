1363e936ccfcc55d4fb8b4c6bbd1168413112196
fangweiye <fangweiye@baidu.com>
2024-06-10 17:57:59 +0800

    feat: change spcal

--- pkg/webserver/webserver.go
+++ pkg/webserver/webserver.go
@@ -1,6 +1,7 @@
 package webserver
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 	"os"
@@ -12,7 +13,6 @@ import (
 )
 
 func Run(dir string, addr string, data map[string]any) {
-	tpl := template.New("index")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		path := filepath.Join(dir, r.URL.Path)
 		if stat, err := os.Stat(path); err == nil && !stat.IsDir() {
@@ -25,11 +25,14 @@ func Run(dir string, addr string, data map[string]any) {
 			indexFilePath := filepath.Join(dir, "index.html")
 			content, err := os.ReadFile(indexFilePath)
 			if err != nil {
+				fmt.Println(err.Error())
 				http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 				return
 			}
+			tpl := template.New("index")
 			parser, err := tpl.Parse(*(*string)(unsafe.Pointer(&content)))
 			if err != nil {
+				fmt.Println(err.Error())
 				http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 				return
 			}
@@ -37,6 +40,7 @@ func Run(dir string, addr string, data map[string]any) {
 			w.WriteHeader(http.StatusOK)
 
 			if err = parser.Execute(w, data); err != nil {
+				fmt.Println(err.Error())
 				http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 				return
 			}
@@ -47,6 +51,6 @@ func Run(dir string, addr string, data map[string]any) {
 
 	log.Infof("Starting web server at %s\n", addr)
 	if err := http.ListenAndServe(addr, nil); err != nil {
-		log.Warnf("Failed to start web server: %v", err)
+		panic("Failed to start web server: " + err.Error())
 	}
 }
